c1aee03c30fdbc08170d8dbd77e490246c84105c
maryem <chaabamaryem@gmail.com>
2024-05-25 13:52:18 +0200

    initial commit

---
+++ backend/routes/authRoute.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const { registerUserCtrl, loginUserCtrl, veerifyUserAccountCtr} = require('../controllers/authController')
+
+
+
+// api/auth/register
+router.post('/register',registerUserCtrl)
+
+// /api/auth/:userId/verify/:token
+router.get('/:userId/verify/:token',veerifyUserAccountCtr)
+
+
+module.exports =router;
